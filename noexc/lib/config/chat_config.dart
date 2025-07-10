81abb8047be0311d33a033ed055911ee5f85e425
jipijipi <jp.leflot@gmail.com>
2025-07-11 00:32:05 +0200

    cleaned sequences

--- noexc/lib/config/chat_config.dart
+++ noexc/lib/config/chat_config.dart
@@ -32,11 +32,6 @@ class ChatConfig {
     'tutorial': 'App Tutorial',
     'support': 'Get Help',
     'menu': 'Main Menu',
-    'autoroute_test': 'Autoroute Test',
-    'custom_value_demo': 'Custom Value Demo',
-    'comparison_demo': 'Comparison Operators Demo',
-    'multi_text_demo': 'Multi-Text Message Demo',
-    'test_single_responsibility': 'Single Responsibility Test',
     'comprehensive_test': 'Comprehensive Feature Test',
   };
   
--- noexc/lib/constants/app_constants.dart
+++ noexc/lib/constants/app_constants.dart
@@ -23,7 +23,7 @@ class AppConstants {
   // Chat Sequences
   static const String sequencesAssetPath = 'assets/sequences/';
   static const String defaultSequenceId = 'onboarding';
-  static const List<String> availableSequences = ['onboarding', 'tutorial', 'support', 'menu', 'autoroute_test','custom_value_demo', 'comparison_demo', 'multi_text_demo', 'test_single_responsibility', 'comprehensive_test'];
+  static const List<String> availableSequences = ['onboarding', 'tutorial', 'support', 'menu', 'comprehensive_test'];
   
   // User Response ID Offset
   static const int userResponseIdOffset = 1000;

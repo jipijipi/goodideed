4b85444b9b0c0b12ed869790542a835d0465c3cf
jipijipi <jp.leflot@gmail.com>
2025-07-12 17:15:45 +0200

    Basic nodes

--- noexc/noexc-authoring-tool/src/App.tsx
+++ noexc/noexc-authoring-tool/src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css';
+
+const nodes = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Node 1' },
+    type: 'default',
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Node 2' },
+    type: 'default',
+  },
+];
+
+const edges = [
+  { id: 'e1-2', source: '1', target: '2', type: 'default' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={nodes} edges={edges} />
     </div>
   );
 }

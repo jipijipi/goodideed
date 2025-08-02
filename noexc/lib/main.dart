9a20b51df06c3dc0b6aa954848a24e7874f708dc
jipijipi <jp.leflot@gmail.com>
2025-08-03 00:53:35 +0200

    feat-working: Add RiveArmTestWidget for arm animation and data binding demonstration

--- noexc/lib/main.dart
+++ noexc/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:rive/rive.dart';
 import 'widgets/chat_screen.dart';
 import 'widgets/rive_test_widget.dart';
 import 'widgets/rive_data_binding_test_widget.dart';
+import 'widgets/rive_arm_test_widget.dart';
 import 'services/service_locator.dart';
 import 'services/session_service.dart';
 import 'themes/app_themes.dart';
@@ -78,7 +79,7 @@ class _MyAppState extends State<MyApp> {
       darkTheme: AppThemes.darkTheme,
       themeMode: AppThemes.getThemeMode(_isDarkMode),
       home: _isInitialized 
-        ? const RiveDataBindingTestWidget() // RiveTestWidget() // ChatScreen(onThemeToggle: _toggleTheme)
+        ? const RiveArmTestWidget() // RiveDataBindingTestWidget() // RiveTestWidget() // ChatScreen(onThemeToggle: _toggleTheme)
         : const Scaffold(
             body: Center(
               child: CircularProgressIndicator(),
